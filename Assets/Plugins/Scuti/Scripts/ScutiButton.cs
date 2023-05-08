22d5e38511ec9017fb3a931074bbf4a4cb612038
Mark Grossnickle <markgrossnickle@users.noreply.github.com>
2023-05-08 13:28:07 -0600

    Imported project and dependencies

---
+++ Assets/Plugins/Scuti/Scripts/ScutiButton.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using Scuti;
+using System;
+using UnityEngine.EventSystems;
+
+public class ScutiButton : MonoBehaviour
+{
+    public GameObject NotificationIcon;
+    public GameObject NewItems;
+
+    private bool _started;
+
+    private void Awake()
+    {
+        NotificationIcon.SetActive(true);
+        NewItems?.SetActive(false);
+    }
+
+    public void Start()
+    {
+        ScutiSDK.Instance.OnNewProduct += OnNewProducts;
+        ScutiSDK.Instance.OnNewReward += OnNewRewards;
+        _started = true;
+        ScutiSDK.Instance.RequestNew();
+        //EventSystem.current.SetSelectedGameObject(null);
+        //EventSystem.current.SetSelectedGameObject(Button);
+    }
+
+    public void OnEnable()
+    {
+        if(_started)
+            ScutiSDK.Instance.RequestNew();
+    }
+     
+
+    public void OnClick()
+    { 
+        ScutiSDK.Instance.ShowStore();
+        NotificationIcon.SetActive(false);
+    }
+
+    private void OnNewProducts(bool value)
+    {
+        NewItems?.SetActive(value);
+    }
+
+
+    private void OnNewRewards(bool value)
+    {
+        NotificationIcon.SetActive(value);
+    }
+
+
+    private void OnDestroy()
+    {
+        if (ScutiSDK.Instance != null)
+        {
+            ScutiSDK.Instance.OnNewProduct -= OnNewProducts;
+            ScutiSDK.Instance.OnNewReward -= OnNewRewards;
+        }
+    }
+
+
+}
